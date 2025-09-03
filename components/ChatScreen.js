171f331cef3368c6993f9b0c05d7199d869245d6
agawi88 <agnessn@icloud.com>
2025-09-04 01:39:59 +0200

    first code with giftedChat

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -1,31 +1,68 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Button, StyleSheet, View, Text } from 'react-native';
+import { GiftedChat } from "react-native-gifted-chat";
+
 
 const Screen2 = ({ route, navigation }) => {
 
   const { name, backgroundColor } = route.params;
+  const [messages, setMessages] = useState([]); // because the chat needs to send, receive and display messages, so their state will be changing
+  const onSend = (newMessages) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+  }
+  // Chat code for messages from GiftedChat
 
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+    ]);
+  }, []);
+
+  // Navigating between pages
     useEffect(() => {
-        navigation.setOptions({ title: name }, {backgroundColor: backgroundColor});
+      navigation.setOptions({
+        title: name,
+        headerStyle: { backgroundColor: backgroundColor },
+      });
     }, []);
 
   return (
     <View style={[styles.container, { backgroundColor: backgroundColor || 'white' }]}>
       <Text>Hello ChatScreen!</Text>
+      <View style={styles.topButton}>
         <Button
        title="Go back to StartScreen"
        onPress={() => navigation.navigate('Screen1')}
-       />
-   </View>
- );
+        />
+      </View>
+    <GiftedChat
+     messages={messages}
+     onSend={messages => onSend(messages)}
+     user={{
+       _id: 1
+     }}
+   />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
  container: {
    flex: 1,
+  },
+  topButton: {
    justifyContent: 'center',
-   alignItems: 'center'
- }
+   alignItems: 'center',
+  }
 });
 
 export default Screen2;
